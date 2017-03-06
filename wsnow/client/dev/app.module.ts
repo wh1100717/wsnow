7c4f27eeb895aa44f6d0347345a5af52ac555f30
EricWang <wangh@ricequant.com>
2017-03-06 15:49:56 +0800

    add Angular hello world

---
+++ wsnow/client/dev/app.module.ts
@@ -0,0 +1,23 @@
+import {
+  NgModule
+} from "@angular/core"
+import {
+  BrowserModule
+} from "@angular/platform-browser"
+import {
+  App
+} from "./app"
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    App
+  ],
+  providers: [],
+  bootstrap: [
+    App
+  ]
+})
+export class AppModule {}
---
+++ wsnow/client/dev/app.ts
@@ -0,0 +1,13 @@
+import {
+  Component
+} from "@angular/core"
+
+@Component({
+  selector: "app",
+  template: `
+    Hello World
+  `
+})
+export class App {
+  
+}
